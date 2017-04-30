e1ababcd961a2a05a00d72235b989d41b355eebe
FatimaJabeen <BSEF15M512@pucit.edu.pk>
2017-04-30 09:31:30 -0700

    Create FCFS.py

---
+++ FCFS.py
@@ -0,0 +1,19 @@
+#!user/bin/pyton
+total_wtime = 0
+n = int(raw_input('Enter the total numbers of processes: '))
+for i in xrange(n):
+    process_queue.append([])#append a list object to the list
+    process_queue[i].append(raw_input('Enter process name: '))
+    process_queue[i].append(int(raw_input('Enter process arrival Time : ')))
+    total_wtime += process_queue[i][1]
+    process_queue[i].append(int(raw_input('Enter Process CPU bustTime : ')))
+    print ''
+
+process_queue.sort(key = lambda process_queue:process_queue[1])
+
+print 'ProcessName\tArrivalTime\tBurstTime'
+for i in xrange(n):
+    print process_queue[i][0],'\t\t',process_queue[i][1],'\t\t',process_queue[i][2]
+
+print 'Total waiting time: ',total_wtime
+print 'Average waiting time: ',(total_wtime/n)
