c5a599f45fc4ab581f34ed4c00d3b21aa11092c1
FatimaJabeen <BSEF15M512@pucit.edu.pk>
2017-05-07 18:34:21 -0700

    Update priority Queue.py

--- priority Queue.py
+++ priority Queue.py
@@ -1,32 +1,25 @@
-#!usr/bin/env python
-
 total = 0
 priority=[]
 processes={}
 burst_time=[]
 arrival_time=[]
-n = input("Enter the number of processes:")
+n = int(input("Enter the number of processes:"))
 
-for i in range (0,n):
-	prio_no=input("Priority number:")
-	priority.append(prio_no)
-	a_time=input("Arrival Time:")
+for i in range (n):
+	priority_no=int(input("Priority number:"))
+	priority.append(priority_no)
+	a_time=int(input("Arrival Time:"))
 	arrival_time.append(a_time)
-        b_time=input("Burst Time:")
+        b_time=int(input("Burst Time:"))
 	burst_time.append(b_time)
 	processes[priority[i]] = [i+1 , arrival_time[i] , burst_time[i]]
 	print "Priorityno. "\t" Arrival Time "\t" Burst Time"
-
-for i in range (0,n):
-	print priority[i] , "\t\t\t" , arrival_time[i], "\t\t\t" , burst_time[i] 
+for i in range (n):
+	print priority[i] , "\t\t" , arrival_time[i], "\t\t" , burst_time[i] 
 	priority.sort()
-	total = processes.get(priority[0])[1]
-	min = total
-        if(total>0):
-	print  total
-		 
-for i in range (0,n):
+	total = processes.get(priority[0])[1]		 
+for i in range (n):
 	total = total + processes.get(priority[i])[2]
-	print min , "\t" , total
-	min = total
+	print  total
+
 
